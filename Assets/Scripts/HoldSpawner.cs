425922500d839d2cdc8f23d2c281948efb57ada8
Raghav Narula <raghavnarula.77@gmail.com>
2023-09-05 05:29:51 -0700

    init project. holds can be added, removed, moved, rotated, and resized. Can set holds for arms or feet, and force readings in progress

---
+++ Assets/Scripts/HoldSpawner.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform prefabHold;
+    private HashSet<Transform> holds;
+    private SelectHolds selectHolds;
+
+    void Awake()
+    {
+        holds = new HashSet<Transform>();
+        selectHolds = transform.GetComponent<SelectHolds>();
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform hold = transform.GetChild(i);
+            holds.Add(hold);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            AddHold();
+        if (Input.GetKeyDown(KeyCode.S))
+            RemoveHold();
+    }
+
+    public void AddHold()
+    {
+        Transform hold = Instantiate(prefabHold, transform.position, Quaternion.identity, transform);
+        holds.Add(hold);
+    }
+
+
+    public void RemoveHold()
+    {
+        Transform hold = selectHolds.selectedHold;
+        if (!hold || !holds.Contains(hold))
+            return;
+
+        holds.Remove(hold);
+        Destroy(hold.gameObject);
+    }
+}
---
+++ Assets/Scripts/SelectHolds.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SelectHolds : MonoBehaviour
+{
+    [SerializeField] private Camera camera;
+    [SerializeField] private TextMeshProUGUI modeTxt;
+
+    public Transform selectedHold { get; private set; }
+    private HoldSettings selectedHoldSettings;
+    private LayerMask holdsLM = (1 << 6);
+
+    private bool isDragging;
+    private bool enableRotationMode;
+    private Vector3 initialDragOffset;
+    private const float RESIZE_SPEED = 0.6f;
+
+    void Start()
+    {
+        modeTxt.text = "Mode: Movement";
+    }
+
+    void Update()
+    {
+        selectHold();
+        dragHold();
+        rotateHold();
+        resizeHold();
+
+        if (Input.GetKeyDown(KeyCode.R))
+            enableRotationMode = !enableRotationMode;
+
+        // keys to change the selected hold's limb type
+        if (selectedHold)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+                selectedHoldSettings.SetToArmType();
+            else if (Input.GetKeyDown(KeyCode.W))
+                selectedHoldSettings.SetToFeetType();
+        }
+    }
+
+    // select a hold (or deselect a hold if clicking nothing)
+    private void selectHold()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 100, holdsLM))
+            {
+                selectedHoldSettings?.EnableOutline(false);
+                selectedHold = hit.transform.parent;
+                Debug.Log(selectedHold);
+                selectedHoldSettings = selectedHold.GetComponent<HoldSettings>();
+                selectedHoldSettings.EnableOutline(true);
+
+                isDragging = true;
+                initialDragOffset = camera.ScreenToWorldPoint(Input.mousePosition) - selectedHold.transform.position;
+            }
+            else
+            {
+                selectedHoldSettings?.EnableOutline(false);
+                selectedHold = null;
+            }
+        }
+    }
+
+    private void dragHold()
+    {
+        if (!selectedHold)
+            return;
+
+        if (isDragging && Input.GetMouseButton(0))
+            selectedHold.position = camera.ScreenToWorldPoint(Input.mousePosition) - initialDragOffset;
+
+        if (Input.GetMouseButtonUp(0))
+            isDragging = false;
+    }
+
+    private void rotateHold()
+    {
+        if (!selectedHold)
+            return;
+
+        if (Input.GetMouseButton(1) || enableRotationMode)
+        {
+            Vector3 worldMousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 dir = new Vector2(worldMousePos.x - selectedHold.position.x, worldMousePos.y - selectedHold.position.y);
+            selectedHold.up = dir;
+
+            modeTxt.text = "Mode: Rotation";
+        }
+        else
+            modeTxt.text = "Mode: Movement";
+    }
+
+    private void resizeHold()
+    {
+        if (!selectedHold)
+            return;
+
+        Vector3 localScale = selectedHold.transform.GetChild(0).localScale;
+        float newXScale = localScale.x + Input.mouseScrollDelta.y * RESIZE_SPEED;
+        selectedHold.transform.GetChild(0).localScale = new Vector3(newXScale, localScale.y, localScale.z);
+    }
+}
